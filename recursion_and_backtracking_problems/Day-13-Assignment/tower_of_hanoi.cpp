456dc8d0f086229aafca281c0ae658091876b599
rishabg30 <rishab30workmail@gmail.com>
2024-09-14 19:49:27 +0530

    solved tower of hanoi recursion problem

--- recursion_and_backtracking_problems/Day-13-Assignment/tower_of_hanoi.cpp
+++ recursion_and_backtracking_problems/Day-13-Assignment/tower_of_hanoi.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+void toh_calculate(int n, int src, int helper, int dest)
+{
+
+    // Base Condition
+    if (n == 1)
+    {
+        cout << "move disk " << n << " from rod " << src << " to rod " << dest << endl;
+        return;
+    }
+    // Move n-1 disks first from src -> helper
+    toh_calculate(n - 1, src, dest, helper);
+    // Print the disk
+    cout << "move disk " << n << " from rod " << src << " to rod " << dest << endl;
+    toh_calculate(n - 1, helper, src, dest);
+}
 long long toh(int n, int from, int to, int aux)
 {
+    toh_calculate(n, from, aux, to);
+
+    // Number of steps will be 2^N -1
+    return pow(2, n) - 1;
 }
 signed main()
 {
@@ -11,8 +30,8 @@ signed main()
 #endif
 
     /*
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(2^N)
+    Space complexity: O(N)
     */
     int n, from, to, aux;
     cin >> n >> from >> to >> aux;
