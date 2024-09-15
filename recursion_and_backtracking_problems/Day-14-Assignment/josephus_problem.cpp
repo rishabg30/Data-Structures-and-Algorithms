e58fcc98e5592f5a60ec8de38fb6369dd9ae678f
rishabg30 <rishab30workmail@gmail.com>
2024-09-15 12:55:06 +0530

    solved recursion problems day - 6

--- recursion_and_backtracking_problems/Day-14-Assignment/josephus_problem.cpp
+++ recursion_and_backtracking_problems/Day-14-Assignment/josephus_problem.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+int winner(int n, int k)
+{
+    if (n == 1)
+    {
+        // If only 1 member is left, it is treated as 0
+        return 0;
+    }
+    // competitors are of range 0...n-1
 
+    int x = winner(n - 1, k);
+    int y = (x + k) % n;
+
+    return y;
+}
+int josephus(int n, int k)
+{
+    return winner(n, k) + 1; // 1 is added as it is 1 based numbers
+}
 signed main()
 {
 #ifndef ONLINE_JUDGE
@@ -9,7 +26,10 @@ signed main()
 #endif
 
     /*
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(N)
+    Space complexity: O(N)
     */
+    int n, k;
+    cin >> n >> k;
+    cout << josephus(n, k) << endl;
 }
