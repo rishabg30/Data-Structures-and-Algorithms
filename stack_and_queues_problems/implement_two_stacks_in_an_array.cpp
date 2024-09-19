a0a2e0c79c81589c88f239b22412a8904f084a76
rishabg30 <rishab30workmail@gmail.com>
2024-09-19 19:52:02 +0530

    solved stack constructive problems

--- stack_and_queues_problems/implement_two_stacks_in_an_array.cpp
+++ stack_and_queues_problems/implement_two_stacks_in_an_array.cpp
@@ -1,6 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
+class twoStacks
+{
+public:
+    int *arr;
+    int start_ptr;
+    int end_ptr = 0;
+    int size;
+    twoStacks(int n = 100)
+    {
+        arr = new int[n];
+        start_ptr = -1,
+        end_ptr = n;
+        size = n;
+    }
+    // Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        start_ptr++;
+        if (start_ptr == end_ptr)
+        {
+            return;
+        }
+        else
+        {
+            arr[start_ptr] = x;
+        }
+    }
 
+    // Function to push an integer into the stack2.
+    void push2(int x)
+    {
+        end_ptr--;
+        if (start_ptr == end_ptr)
+        {
+            return;
+        }
+        else
+        {
+            arr[end_ptr] = x;
+        }
+    }
+
+    // Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        if (start_ptr == -1)
+        {
+            return -1;
+        }
+        int x = arr[start_ptr];
+        start_ptr--;
+        return x;
+    }
+
+    // Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        if (end_ptr == size)
+        {
+            return -1;
+        }
+        int x = arr[end_ptr];
+        end_ptr++;
+        return x;
+    }
+};
 signed main()
 {
 #ifndef ONLINE_JUDGE
@@ -9,7 +74,7 @@ signed main()
 #endif
 
     /*
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(N)
+    Space complexity: O(N)
     */
 }
--- stack_and_queues_problems/n_stacks_in_an_array.cpp
+++ stack_and_queues_problems/n_stacks_in_an_array.cpp
@@ -1,6 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
+class ListNode
+{
+public:
+    int index;
+    ListNode *next;
+    ListNode(int x)
+    {
+        index = x;
+        next = NULL;
+    }
+};
+class NStack
+{
+public:
+    int *arr; // original data
+    /*
+    top element index of particular stack. we created this double pointer because
+    int *arr=new int[]; itself is a pointer pointing to integer type value stored in array.
+    To point this we need a double pointer.
+    */
+    ListNode **top;
+    stack<int> st; // to keep track of empty indexes
 
+    // N -> number of stacks, S -> size of array
+    NStack(int N, int S)
+    {
+        arr = new int[S];
+        top = new ListNode *[N];
+
+        // Initialized NULL to every linkedlist array
+        for (int i = 0; i < N; i++)
+        {
+            top[i] = NULL;
+        }
+        // Pushed indexes available right now
+        for (int i = 0; i < S; i++)
+        {
+            st.push(i);
+        }
+    }
+
+    // Pushes 'X' into the Mth stack. Returns true if it gets pushed into the stack, and false otherwise.
+    bool push(int x, int m)
+    {
+        // Base Condition : if stack is empty, means no index left -> array full
+        if (st.size() == 0)
+        {
+            return false;
+        }
+        // Storing value in original array
+        arr[st.top()] = x;
+        ListNode *temp = new ListNode(st.top());
+
+        // Pointing to upwards direction
+        temp->next = top[m - 1];
+        top[m - 1] = temp;
+        st.pop();
+        return true;
+    }
+
+    // Pops top element from Mth Stack. Returns -1 if the stack is empty, otherwise returns the popped element.
+    int pop(int m)
+    {
+        // Base Condition : if linkedlist array from which we have to pop is empty
+        if (top[m - 1] == NULL)
+        {
+            return false;
+        }
+        // After popping, this index becomes empty, push it inside stack
+        int element = arr[top[m - 1]->index];
+        st.push(top[m - 1]->index);
+
+        // Remove connection
+        top[m - 1] = top[m - 1]->next;
+        return element;
+    }
+};
 signed main()
 {
 #ifndef ONLINE_JUDGE
@@ -9,7 +85,7 @@ signed main()
 #endif
 
     /*
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(N)
+    Space complexity: O(N + S)
     */
 }
