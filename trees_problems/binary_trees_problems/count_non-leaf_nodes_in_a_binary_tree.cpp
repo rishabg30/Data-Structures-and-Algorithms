6f09ee533d9515a7f1099195168664767db405de
rishabg30 <rishab30workmail@gmail.com>
2024-09-23 18:18:24 +0530

    added easy problems of binary trees

---
+++ trees_problems/binary_trees_problems/count_non-leaf_nodes_in_a_binary_tree.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
+    {
+        this->val = val;
+        left = right = NULL;
+    }
+};
+int countNonLeafNodes(TreeNode *root)
+{
+    // Base Condition
+    if (root == NULL)
+    {
+        return 0;
+    }
+    // Base condition for a leaf node
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 0;
+    }
+    // Non Leaf nodes in left binary tree
+    int count_left_non_leaf_nodes = countNonLeafNodes(root->left);
+    // Non Leaf nodes in right binary tree
+    int count_right_non_leaf_nodes = countNonLeafNodes(root->right);
+    return count_left_non_leaf_nodes + count_right_non_leaf_nodes + 1;
+}
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    /*
+    Time complexity: O(N)
+    Space complexity: O(N)
+    */
+    TreeNode *root = new TreeNode(1);
+    TreeNode *node1 = new TreeNode(2);
+    TreeNode *node2 = new TreeNode(7);
+    root->left = node1;
+    root->right = node2;
+
+    TreeNode *node3 = new TreeNode(3);
+    TreeNode *node4 = new TreeNode(6);
+    TreeNode *node5 = new TreeNode(8);
+    node1->left = node3;
+    node1->right = node4;
+    node2->right = node5;
+
+    TreeNode *node6 = new TreeNode(4);
+    TreeNode *node7 = new TreeNode(5);
+    TreeNode *node8 = new TreeNode(9);
+    TreeNode *node9 = new TreeNode(10);
+    node3->left = node6;
+    node3->right = node7;
+    node5->left = node8;
+    node5->right = node9;
+    cout << countNonLeafNodes(root) << endl;
+}
---
+++ trees_problems/binary_trees_problems/size_of_binary_tree.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
+    {
+        this->val = val;
+        left = right = NULL;
+    }
+};
+int getSize(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    // Left size of binary tree
+    int leftSize = getSize(root->left);
+    // Right size of binary tree
+    int rightSize = getSize(root->right);
+    return leftSize + rightSize + 1; // 1 added for the root node
+}
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    /*
+    Time complexity: O(N)
+    Space complexity: O(N)
+    */
+    TreeNode *root = new TreeNode(1);
+    TreeNode *node1 = new TreeNode(2);
+    TreeNode *node2 = new TreeNode(7);
+    root->left = node1;
+    root->right = node2;
+
+    TreeNode *node3 = new TreeNode(3);
+    TreeNode *node4 = new TreeNode(6);
+    TreeNode *node5 = new TreeNode(8);
+    node1->left = node3;
+    node1->right = node4;
+    node2->right = node5;
+
+    TreeNode *node6 = new TreeNode(4);
+    TreeNode *node7 = new TreeNode(5);
+    TreeNode *node8 = new TreeNode(9);
+    TreeNode *node9 = new TreeNode(10);
+    node3->left = node6;
+    node3->right = node7;
+    node5->left = node8;
+    node5->right = node9;
+    cout << getSize(root) << endl;
+}
