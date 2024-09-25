756dc0bf039c0f2b29cc52b0e2de055f0862cbf7
rishabg30 <rishab30workmail@gmail.com>
2024-09-25 11:52:20 +0530

    added construction set of binary trees problems

--- trees_problems/binary_trees_problems/construct_binary_tree_from_inOrder_and_postOrder.cpp
+++ trees_problems/binary_trees_problems/construct_binary_tree_from_inOrder_and_postOrder.cpp
@@ -1,1 +1,65 @@
-ds
+#include <bits/stdc++.h>
+using namespace std;
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
+    {
+        this->val = val;
+        left = right = NULL;
+    }
+};
+int findPosition(int target, int inOrder[], int inOrderStart, int inOrderEnd)
+{
+    for (int i = inOrderStart; i <= inOrderEnd; i++)
+    {
+        if (inOrder[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+TreeNode *myTree(int inOrder[], int postOrder[], int inOrderStart, int inOrderEnd, int index)
+{
+    // Terminating condition
+    if (inOrderStart > inOrderEnd)
+    {
+        return NULL;
+    }
+    // Step 1-> Create a root node from postOrder array
+    TreeNode *root = new TreeNode(postOrder[index]);
+    // Step 2-> Find out the position of root node in inOrder array
+    int position = findPosition(root->val, inOrder, inOrderStart, inOrderEnd);
+
+    // For postOrder, we have to first go to the right side and buildTree and then to the left side
+    // Build right tree
+    root->right = myTree(inOrder, postOrder, position + 1, inOrderEnd, index - 1);
+    // Build left tree
+    root->left = myTree(inOrder, postOrder, inOrderStart, position - 1, index - (inOrderEnd - position) - 1);
+
+    return root;
+}
+TreeNode *buildTree(int n, int inOrder[], int postOrder[])
+{
+    // Here index will start from reverse order
+    return myTree(inOrder, postOrder, 0, n - 1, n - 1);
+}
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    /*
+    Time complexity: O(N * N)
+    Space complexity: O(N)
+    */
+    int inOrder[5] = {9, 5, 2, 3, 4};
+    int postOrder[5] = {5, 9, 3, 4, 2};
+    TreeNode *root = buildTree(5, inOrder, postOrder);
+}
--- trees_problems/binary_trees_problems/construct_binary_tree_from_inOrder_and_preOrder.cpp
+++ trees_problems/binary_trees_problems/construct_binary_tree_from_inOrder_and_preOrder.cpp
@@ -12,8 +12,38 @@ public:
         left = right = NULL;
     }
 };
+int findPosition(int target, int inOrder[], int inOrderStart, int inOrderEnd)
+{
+    for (int i = inOrderStart; i <= inOrderEnd; i++)
+    {
+        if (inOrder[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+TreeNode *myTree(int inOrder[], int preOrder[], int inOrderStart, int inOrderEnd, int index)
+{
+    // Terminating condition
+    if (inOrderStart > inOrderEnd)
+    {
+        return NULL;
+    }
+    // Step 1-> Create a root node from preOrder array
+    TreeNode *root = new TreeNode(preOrder[index]);
+    // Step 2-> Find out the position of root node in inOrder array
+    int position = findPosition(root->val, inOrder, inOrderStart, inOrderEnd);
+
+    // Build left tree
+    root->left = myTree(inOrder, preOrder, inOrderStart, position - 1, index + 1);
+    // Build right tree
+    root->right = myTree(inOrder, preOrder, position + 1, inOrderEnd, index + (position - inOrderStart) + 1);
+    return root;
+}
 TreeNode *buildTree(int inOrder[], int preOrder[], int n)
 {
+    return myTree(inOrder, preOrder, 0, n - 1, 0);
 }
 signed main()
 {
@@ -23,11 +53,10 @@ signed main()
 #endif
 
     /*
-    Time complexity: O()
-    Space complexity: O()
+    Time complexity: O(N * N)
+    Space complexity: O(N)
     */
-    int inOrder = {1, 6, 8, 7};
-    int preOrder = {1, 6, 7, 8};
+    int inOrder[4] = {1, 6, 8, 7};
+    int preOrder[4] = {1, 6, 7, 8};
     TreeNode *tree = buildTree(inOrder, preOrder, 4);
-    
 }
